a6964f4d8a9df9d72869d134d1749b41ea110606
GongXian Cao <gongxiancao@hotmail.com>
2016-03-10 14:41:50 +0800

    add error codes

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  '-1' : 'system busy, retry later',
+  0    : 'success',
+  1    : 'internal error, contact us',
+  10007: 'invalid token',
+  10008: 'token is expired',
+  10010: 'require token',
+  11001: 'require username',
+  11002: 'require password',
+  11003: 'user not exist',
+  11004: 'password not match',
+  11005: 'failed to generate token, contact us',
+  11006: 'failed to create user',
+  11007: 'failed to hash password',
+  11008: 'failed to save user, please check if required field is correct'
+};
